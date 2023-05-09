4564535029b4ad35dbfc335b888e04d279240511
SnunesA <snunesadiers@gmail.com>
2023-05-09 16:53:14 -0300

    listar

--- cliente.py
+++ cliente.py
@@ -1,17 +1,47 @@
 import requests
-
+from time import sleep
 URL = "http://127.0.0.1:8000"
 
-while True:
-    nome = input("Digite o nome: ")
-    if nome == "":
-        break
-    idade = input("Digite a idade: ")
+print('Seja Bem-vindo a lista de tarefas')
+op=-1
+while op != 0:
+    print('Escolha a opção que deseja:')
+    print('0.Sair 1.Cadastrar - 2.Listar')
+    op = int(input())
+    #Fecha a execução
+    if op == 0: break
+    # Cadastrar
+    if op == 1:
+        x = int(input('Quantas tarefas deseja cadastrar? '))
+        for c in range(x):
+            nome=input("Nome da tarefa:")
+            descricao=input("Descrição:")
+            data=input('Data (DD/MM/AA):')
+            registro = {
+            "nome": nome, 
+            "descricao": descricao,
+            "data":data
+            }
+            response = requests.post(f"{URL}/cadastrar", json=registro)
+            print("Tarefa cadastrada!")
+            sleep(1)
+    # Listar
+    if op == 2:
+        response= requests.get(f"{URL}/listar")
+        dados=response.json()
+        
+        if len(dados) == 0: 
+            print('Nenhuma Tarefa na Lista')
+        for i in dados:
+            for c in dados[i]:     
+                print(dados[i][c])
+            print('')
+            
 
-    registro = {
-        "nome": nome, 
-        "idade": idade
-    }
+       
+        
 
-    response = requests.post(f"{URL}/cadastrar", json=registro)
+
+
+    
    
--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 from fastapi import FastAPI, Request
+#Fastapi é um framework web de alta performance para construir APIs com suporte a tipagem de dados. 
+# Request: é uma classe do módulo fastapi que representa uma requisição HTTP recebida pela aplicação.
 import json 
 
 app = FastAPI()
-agenda = dict()
+tarefas = dict()
 id_count = 0
 
 # Métodos CRUD
+# Quando um cliente envia uma requisição HTTP POST para /cadastrar, a função cadastrar que recebe um objeto Request como argumento é executada.
 @app.post("/cadastrar")
 async def cadastrar(request: Request):
     global id_count
-    global agenda
-
+    global tarefas
+    # cria um novo identificador único para cada tarefa.
     id_ = id_count
     id_count += 1
-    
+    # lê os dados enviados pelo cliente no corpo da requisição HTTP
     dados = await request.json()
-    agenda[id_] = dados
-    print(agenda)
-    return {"id": id_}
-
+    tarefas[id_] = dados
+    print(tarefas)
+     
+# Quando um cliente envia uma requisição HTTP GET para /listar, a função listar é executada.
 @app.get('/listar')
 async def list():
-    return agenda
+    return tarefas
 
-# Métodos de salvamento
+# Define a função salvar para ser executada quando o servidor é encerrado.
 @app.on_event("shutdown")
 async def salvar():
-    global agenda
-    print(agenda)
+    global tarefas
+    print(tarefas)
     with open("dados.json", "w") as file:
-        json.dump(agenda, file)
+        json.dump(tarefas, file)
 
+# Define a função carregar para ser executada quando o servidor é iniciado.
 @app.on_event("startup")
 async def carregar():
-    global agenda
+    global tarefas
     global id_count
     try:
         with open("dados.json", "r") as file:
-            agenda = json.load(file)
-        for key in agenda:
+            tarefas = json.load(file)
+        # Percorre as chaves do dicionário agenda para encontrar o maior identificador único já usado e atualiza a variável id_count
+        for key in tarefas:
             if int(key) > id_count:
                 id_count = int(key)
         id_count += 1
